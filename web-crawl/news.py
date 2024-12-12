5e73a451f7a1748b4779215acf772422790f53b5
LovellR <129665363+LovellR@users.noreply.github.com>
2024-12-12 21:24:36 +0900

    move and add google

--- web-crawl/news.py
+++ web-crawl/news.py
@@ -10,11 +10,13 @@ options.add_argument('user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6)
 
 driver = webdriver.Chrome('chromedriver-win64\chromedriver.exe', chrome_options=options)
 
-search_query = "걸그룹 성추행"
+search_query = "수상한 그녀 드라마"
 base_url = f"https://search.naver.com/search.naver?where=news&query={search_query}&sm=tab_opt&sort=0&photo=0&field=0&pd=4&ds=&de=&docid=&related=0&mynews=0&office_type=0&office_section_code=0&news_office_checked=&nso=so%3Ar%2Cp%3A1d&is_sug_officeid=0&office_category=0&service_area=0"
 driver.get(base_url)
 
 news_count = len(driver.find_elements(By.XPATH, '/html/body/div[3]/div[2]/div[1]/div[1]/section/div[1]/div[2]/ul/li'))
+
+print(news_count)
 
 for index in range(1, news_count + 1):
     xpath = f'/html/body/div[3]/div[2]/div[1]/div[1]/section/div[1]/div[2]/ul/li[{index}]/div[1]/div/div[2]/a[2]'
