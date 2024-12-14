29a5fa5fa37fd51e5fbf70bb69d69f2a3890a982
LovellR <129665363+LovellR@users.noreply.github.com>
2024-12-14 14:51:16 +0900

    move and organize files

--- Collector.py
+++ Collector.py
@@ -1,6 +1,6 @@
-from keyword_collector import collect_keywords
-from news_collector import collect_news
-from summarize_collector import collect_summary
+from collector.keyword_collector import collect_keywords
+from collector.news_collector import collect_news
+from collector.summarize_collector import collect_summary
 
 if __name__ == '__main__':
     collect_keywords()
