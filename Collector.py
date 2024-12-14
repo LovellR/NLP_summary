a704bb998fb2c9dcbcd506ad1495118db74f1869
LovellR <129665363+LovellR@users.noreply.github.com>
2024-12-14 13:46:28 +0900

    wrote collector main

---
+++ Collector.py
@@ -0,0 +1,8 @@
+from keyword_collector import collect_keywords
+from news_collector import collect_news
+from summarize_collector import collect_summary
+
+if __name__ == '__main__':
+    collect_keywords()
+    collect_news()
+    collect_summary()
